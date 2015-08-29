430e1084e5e41bda7ba3875e5b1731762a454011
sugarshin <shinsugar@gmail.com>
2015-08-29 23:50:20 +0900

    es6

---
+++ src/partition-size.js
@@ -0,0 +1,30 @@
+/*!
+ * @license partition-size
+ * (c) sugarshin
+ * License: MIT
+ */
+
+'use strict';
+
+module.exports = function partitionSize(array, size) {
+  if (!Array.isArray(array)) {
+    throw new TypeError('should be an Array.');
+  }
+
+  if (typeof size !== 'number' || size <= 0) {
+    return array;
+  }
+
+  if (array.length <= size) {
+    return [array];
+  }
+
+  return array.reduce((result, current, i) => {
+    if (i % size === 0) {
+      result.push([current]);
+    } else {
+      result[result.length - 1].push(current);
+    }
+    return result;
+  }, []);
+};
