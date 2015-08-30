9970c0fa7f3799d06a808daafe12d58ee76e8d8a
sugarshin <shinsugar@gmail.com>
2015-08-30 16:57:47 +0900

    edit test

--- test/test.js
+++ test/test.js
@@ -4,57 +4,54 @@ import partitionSize from '../';
 
 describe('partitionSize', () => {
 
-  let arr = null;
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  beforeEach(done => {
-    arr = [1, 2, 3, 4, 5, 6, 7, 8];
-    done();
+  it('length 8 / size 4', () => {
+    const expected = [[1, 2, 3, 4], [5, 6, 7, 8]];
+    const actual = partitionSize(arr, 4);
+    assert.deepEqual(actual, expected, 'length 8 / size 4');
   });
 
-  it('array length 8 / size 4', () => {
-    const expected = partitionSize(arr, 4);
-    assert(expected.length === 2);
-    assert(Array.isArray(expected[0]));
-    assert(Array.isArray(expected[1]));
-    assert(expected[0].length === 4);
-    assert(expected[1].length === 4);
-    assert(expected[0].every((el, i) => el === arr[i]));
-    assert(expected[1].every((el, i) => el === arr[i + 4]));
+  it('length 8 / size 6', () => {
+    const expected = [[1, 2, 3, 4, 5, 6], [7, 8]];
+    const actual = partitionSize(arr, 6);
+    assert.deepEqual(actual, expected, 'length 8 / size 6');
   });
 
-  it('array length 8 / size 6', () => {
-    const expected = partitionSize(arr, 6);
-    assert(expected.length === 2);
-    assert(Array.isArray(expected[0]));
-    assert(Array.isArray(expected[1]));
-    assert(expected[0].length === 6);
-    assert(expected[1].length === 2);
-    assert(expected[0].every((el, i) => el === arr[i]));
-    assert(expected[1].every((el, i) => el === arr[i + 6]));
+  it('length 8 / size 8', () => {
+    const expected = [[1, 2, 3, 4, 5, 6, 7, 8]];
+    const actual = partitionSize(arr, 8);
+    assert.deepEqual(actual, expected, 'length 8 / size 8');
   });
 
-  it('array length 8 / size 9', () => {
-    const expected = partitionSize(arr, 9);
-    assert(expected.length === 1);
-    assert(expected[0].every((el, i) => el === arr[i]));
+  it('length 8 / size 1', () => {
+    const expected = [[1], [2], [3], [4], [5], [6], [7], [8]];
+    const actual = partitionSize(arr, 1);
+    assert.deepEqual(actual, expected, 'length 8 / size 1');
   });
 
-  it('array length 8 / size 0', () => {
-    const expected = partitionSize(arr, 0);
-    assert(expected.length === 8);
-    assert(expected.every((el, i) => el === arr[i]));
+  it('length 8 / size 9', () => {
+    const expected = [[1, 2, 3, 4, 5, 6, 7, 8]];
+    const actual = partitionSize(arr, 9);
+    assert.deepEqual(actual, expected, 'length 8 / size 9');
   });
 
-  it('array length 8 / size -1', () => {
-    const expected = partitionSize(arr, -1);
-    assert(expected.length === 8);
-    assert(expected.every((el, i) => el === arr[i]));
+  it('length 8 / size 0', () => {
+    const expected = [1, 2, 3, 4, 5, 6, 7, 8];
+    const actual = partitionSize(arr, 0);
+    assert.deepEqual(actual, expected, 'length 8 / size 0');
+  });
+
+  it('length 8 / size -1', () => {
+    const expected = [1, 2, 3, 4, 5, 6, 7, 8];
+    const actual = partitionSize(arr, -1);
+    assert.deepEqual(actual, expected, 'length 8 / size -1');
   });
 
   it('size not a number', () => {
-    const expected = partitionSize(arr, 'not a number');
-    assert(expected.length === 8);
-    assert(expected.every((el, i) => el === arr[i]));
+    const expected = [1, 2, 3, 4, 5, 6, 7, 8];
+    const actual = partitionSize(arr, 'not a number');
+    assert.deepEqual(actual, expected, 'size not a number');
   });
 
 });
